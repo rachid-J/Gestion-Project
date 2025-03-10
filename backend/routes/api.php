971c5a2d6396f63bf7189d7b2bb1e12593cb85c6
walidaitbaha <walidbaha17@gmail.com>
2025-03-10 18:33:02 +0000

    modify api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,4 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
 
+Route::prefix('project')->group(function(){
+    Route::get('/getProjects', [ProjectController::class, 'get']);
+    Route::post('/createProject', [ProjectController::class, 'create']);
+    Route::put('/updateProject/{id}', [ProjectController::class, 'update']);
+    Route::delete('/deleteProject/{id}', [ProjectController::class, 'delete']);
+});
 
