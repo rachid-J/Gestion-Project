edb33aa3a3198bc9406dde72e31398f7d2cb7cab
walidaitbaha <walidbaha17@gmail.com>
2025-03-11 15:21:16 +0000

    routes created complete

--- Frontend/src/router/Routes.jsx
+++ Frontend/src/router/Routes.jsx
@@ -1,52 +1,53 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import {Welcome} from '../pages/Welcome';
-// import {Dashboard} from '../pages/Dashboard';
-// import {Project} from '../pages/Project';
-// import {Team} from '../pages/Team';
-// import {Task} from '../pages/Task';
-// import {Profile} from '../pages/Profile';
-// import {Loading} from '../pages/Loading';
-// import {NotFound} from '../pages/NotFound';
-// import {Default} from '../layouts/Default';
+import { Dashboard } from '../pages/Dashboard';
+import { Project } from '../pages/Project';
+import { Team } from '../pages/Team';
+import { Task } from '../pages/Task';
+import { Profile } from '../pages/Profile';
+import { Setting } from '../pages/Setting';
+import { Security } from '../pages/Security';
+import { Calendar } from '../pages/Calendar';
+import { Reports } from '../pages/Reports';
+import { NotFound } from '../pages/NotFound';
 import { useSelector } from "react-redux";
-import {Auth} from "../pages/Auth";
-export const Router =()=>{
+import { Auth } from "../pages/Auth";
+import { Default } from "../components/layouts/Default";
 
+export const Router = () => {
     const token = useSelector((state) => state.auth.token);
 
     const guestRoutes = [
-        {
-            path:"/auth",
-            element: <Auth />,
-        },
+        { path: "/auth", element: <Auth /> },
         { path: "*", element: <NotFound /> },
-        { path: "/loadings", element: <Loading /> }
-    ]
+    ];
 
     const authenticatedRoutes = [
         {
+            path: "/",
             element: <Default />,
-            children:[
-                { path: "/loadings", element: <Loading /> },
-                // {
-                //     path:"/",
-                //     element: <Welcome />,
-                //     children:[
-                //         {path:"/dashboard", element: <Dashboard />},
-                //         {path:"/project", element: <Project />},
-                //         {path:"/team", element: <Team />},
-                //         {path:"/task", element: <Task />},
-                //         {path:"/profile", element: <Profile />},
-                //     ]
-                // }
+            children: [
+                { path: "dashboard", element: <Dashboard /> },
+                { path: "task", element: <Task /> },
+                { path: "project", element: <Project /> },
+                { path: "calendar", element: <Calendar /> },
+                { path: "reports", element: <Reports /> },
+                { path: "team", element: <Team /> },
+                {
+                    path: "setting",
+                    element: <Setting />,
+                    children: [
+                        { path: "profile", element: <Profile /> },
+                        { path: "security", element: <Security /> }
+                    ]
+                },
             ]
         },
         { path: "*", element: <NotFound /> },
-    ]
+    ];
 
-    const Routes = token ? authenticatedRoutes() : guestRoutes();
+    const routes = token ? authenticatedRoutes : guestRoutes;
 
-    const router = createBrowserRouter(Routes);
+    const router = createBrowserRouter(routes);
 
     return <RouterProvider router={router} />;
-}
+};
